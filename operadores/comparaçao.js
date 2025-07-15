48c2e1cb349150fcbee550b125c44e29ed045897
Jefersonvalentimvenancio <jeferson.valentim.venancio@gmail.com>
2025-07-15 19:30:23 -0300

    alteraços da aula02

---
+++ operadores/comparaçao.js
@@ -0,0 +1,10 @@
+
+// operadores de comparação
+console.log(5 == 5); // Igualdade (true)
+console.log(5 != 3); // Desigualdade (true)
+console.log(5 === 5); // Igualdade estrita (true)
+console.log(5 !== 3); // Desigualdade estrita (true)
+console.log(5 > 3); // Maior que (true)
+console.log(5 < 3); // Menor que (false)
+console.log(5 >= 5); // Maior ou igual a (true)
+console.log(5 <= 3); // Menor ou igual a (false)
